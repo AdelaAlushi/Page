12f3fdefd71623d4151515dcc941a4458b3d9eee
Adela Alushi <adelaalushi18>
2019-06-14 22:13:13 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    // init Isotope
+    var $grid = $('.grid').isotope({
+        // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on('click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+});
+
+
+
